0485cb78fb9372b10b7e8f1895851f002dac2ef5
Jason Le <Jasonleh99@gmail.com>
2018-03-14 20:15:12 -0700

    Review class created and updated Reviews can now be generated and viewed in the UI

--- app/src/main/java/com/csulb/cecsymemeteam/dinnr/MoreReviewsActivity.java
+++ app/src/main/java/com/csulb/cecsymemeteam/dinnr/MoreReviewsActivity.java
@@ -4,19 +4,40 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
 
 public class MoreReviewsActivity extends AppCompatActivity {
-
+    private int numOfReviews = 0;
+    private ArrayList<Review> reviews = new ArrayList<Review>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_more_reviews);
+
+        LinearLayout scrollView = findViewById(R.id.reviewsList);
+
+        //Grabs 10 reviews (from a database or whatever) and creates it on screen so that the user can view them
+        for(int i = 0; i < 10; i++){
+            reviews.add(new Review());
+            scrollView.addView(reviews.get(i).generateReview(this, numOfReviews), new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            numOfReviews++;
+        }
     }
 
+    /**
+     * directs users back to the restaurant profile activity
+     * @param v
+     */
     public void goBack(View v){
         startActivity(new Intent(this, MainActivity.class));
     }
 
+    /**
+     * directs users over to the write review activity
+     * @param v
+     */
     public void writeReview(View v){
         startActivity(new Intent(this, WriteReviewActivity.class));
     }
--- app/src/main/java/com/csulb/cecsymemeteam/dinnr/ProfileActivity.java
+++ app/src/main/java/com/csulb/cecsymemeteam/dinnr/ProfileActivity.java
@@ -1,26 +1,58 @@
 package com.csulb.cecsymemeteam.dinnr;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Space;
+import android.widget.TextView;
+import android.widget.WrapperListAdapter;
+
+import java.util.ArrayList;
 
 public class ProfileActivity extends AppCompatActivity {
-
+    private int numOfReviews = 0;
+    private ArrayList<Review> reviews = new ArrayList<Review>();                  //we need to retrieve this via a database
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        LinearLayout scrollView = findViewById(R.id.reviewsLinearLayout);
+
+        //Grabs 7 reviews (from a database or whatever) and creates it on screen so that the user can view them
+        for(int i = 0; i < 7; i++){
+            reviews.add(new Review());
+            scrollView.addView(reviews.get(i).generateReview(this, numOfReviews), new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            numOfReviews++;
+        }
     }
 
+    /**
+     * Directs users to the write review activity
+     * @param v
+     */
     public void writeReview(View v){
         startActivity(new Intent(this, WriteReviewActivity.class));
     }
 
+    /**
+     * Directs users to another page where they can see reviews more clearly
+     * @param v
+     */
     public void gotoMoreReviews(View v){
         startActivity(new Intent(this, MoreReviewsActivity.class));
     }
 
+    /**
+     * Directs users back to the main activity
+     * @param v
+     */
     public void goBack(View v){
         startActivity(new Intent(this, MainActivity.class));
     }
