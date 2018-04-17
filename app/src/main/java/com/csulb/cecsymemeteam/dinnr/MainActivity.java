eaaa085e4fe89d575928cdc77b254fcef2cf8fc8
Jason <Jasonleh99@gmail.com>
2018-04-17 11:06:24 -0700

    Added swipe functionality

--- app/src/main/java/com/csulb/cecsymemeteam/dinnr/MainActivity.java
+++ app/src/main/java/com/csulb/cecsymemeteam/dinnr/MainActivity.java
@@ -1,6 +1,7 @@
 package com.csulb.cecsymemeteam.dinnr;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.support.annotation.MainThread;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,36 +11,59 @@ import android.view.GestureDetector;
 import android.view.GestureDetector.SimpleOnGestureListener;
 import android.view.MotionEvent;
 import android.view.View.OnTouchListener;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     //private final GestureDetector gestureDetector;
-
+    public RestaurantProfile[] restaurants = new RestaurantProfile[3];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        LinearLayout linearLayout = (LinearLayout) findViewById(R.layout.activity_main);
-
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.main_restaurantPlaque);
+        generateRestaurants(restaurants);
         linearLayout.setOnTouchListener(new GestureListener(MainActivity.this)
         {
+            int index = 0;
             @Override
             public void onSwipeRight()
             {
-                Toast.makeText(MainActivity.this, "swipe right", Toast.LENGTH_SHORT).show();
+                index = (index + 1) % 3;
+                changeProfile(index, restaurants);
             }
             public void onSwipeLeft()
             {
-                Toast.makeText(MainActivity.this, "swipe left", Toast.LENGTH_SHORT).show();
+                index = (index + 1) % 3;
+                changeProfile(index, restaurants);
             }
+
         });
     }
 
+    private void generateRestaurants(RestaurantProfile[] restaurants){
+        restaurants[0] = new RestaurantProfile("Mcdonalds", "0.5 Miles", 4);
+        restaurants[1] = new RestaurantProfile("Taco Bell", "2.0 Miles", 3);
+        restaurants[2] = new RestaurantProfile("Inn N Out", "0.2 Miles", 5);
+    }
 
-
-
+    public void changeProfile(int index, RestaurantProfile[] restaurants){
+        ImageButton imgBtn = (ImageButton) findViewById(R.id.main_profileBtn);
+        if(index == 0){
+            imgBtn.setImageResource(R.drawable.mcdonalds);
+        }
+        else if(index == 1){
+            imgBtn.setImageResource(R.drawable.tacoBell);
+        }
+        else{
+            imgBtn.setImageResource(R.drawable.innNOut);
+        }
+        TextView restaurantDescription = (TextView) findViewById(R.id.main_restaurantDescriptionTxt);
+        restaurantDescription.setText(restaurants[index].getName() + " " + restaurants[index].getDistance());
+    }
 
     public void gotoFavorites(View v){
         startActivity(new Intent(this, FavoritesActivity.class));
