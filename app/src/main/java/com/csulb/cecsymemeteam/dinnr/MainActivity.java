1e2a93f131b591d746bb2922689afb50a054683e
Quang Le <quangle18997@gmail.com>
2018-04-19 01:25:19 -0700

    removed back buttons

--- app/src/main/java/com/csulb/cecsymemeteam/dinnr/MainActivity.java
+++ app/src/main/java/com/csulb/cecsymemeteam/dinnr/MainActivity.java
@@ -3,8 +3,13 @@ package com.csulb.cecsymemeteam.dinnr;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.support.annotation.MainThread;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.content.Context;
 import android.view.GestureDetector;
@@ -16,9 +21,10 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
 
-    //private final GestureDetector gestureDetector;
     public RestaurantProfile[] restaurants = new RestaurantProfile[3];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +32,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         LinearLayout linearLayout = (LinearLayout) findViewById(R.id.main_restaurantPlaque);
         generateRestaurants(restaurants);
+        mDrawerLayout =(DrawerLayout) findViewById(R.id.drawer);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout,R.string.open, R.string.close);
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        //this.getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         linearLayout.setOnTouchListener(new GestureListener(MainActivity.this)
         {
             int index = 0;
@@ -37,13 +52,22 @@ public class MainActivity extends AppCompatActivity {
             }
             public void onSwipeLeft()
             {
-                index = (index + 1) % 3;
+                index = Math.abs(index - 1) % 3;
                 changeProfile(index, restaurants);
             }
 
         });
     }
 
+    /*@Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(mToggle.onOptionsItemSelected(item))
+        {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }*/
     private void generateRestaurants(RestaurantProfile[] restaurants){
         restaurants[0] = new RestaurantProfile("Mcdonalds", "0.5 Miles", 4);
         restaurants[1] = new RestaurantProfile("Taco Bell", "2.0 Miles", 3);
@@ -75,5 +99,22 @@ public class MainActivity extends AppCompatActivity {
 
     public void gotoProfile(View v){
         startActivity(new Intent(this, ProfileActivity.class));
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if(id == R.id.login)
+        {
+           // Toast.makeText(this,"login",Toast.LENGTH_SHORT.show());
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+        if(id == R.id.newAccount)
+        {
+            // Toast.makeText(this,"login",Toast.LENGTH_SHORT.show());
+            startActivity(new Intent(this, NewAccountActivity.class));
+        }
+        return false;
     }
 }
